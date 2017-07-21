b2ac4f019a512055267c832e65ec14c44a75e981
Pei <jzha471@aucklanduni.ac.nz>
2017-07-21 17:57:32 +1200

    10.Action Results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,7 +14,11 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
-            return View( movie);
+            //return View( movie);
+            //return Content("Hello world!");
+            //return HttpNotFound(); 
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
     }
 }
