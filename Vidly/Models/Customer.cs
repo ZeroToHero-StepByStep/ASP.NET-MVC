a03052507e862f28861c35dc8c0d9b1c36e7e483
Pei <jzha471@aucklanduni.ac.nz>
2017-07-22 19:39:38 +1200

    25.Code first Migration (add MembershipType attribut to Customer model and migration)

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,13 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        //MembershipType is called a naviation property becuase it allows us 
+        //to navigate  from one type to another. In this case : Custoemr->Membershiptype
+        public MembershipType MembershipType { get; set; }
+        //sometime for optimization , we don't want to load the entire Membershiptype of the object 
+        //we may only need a foreign key so we can add another property here : MembershpTypeId 
+        //EF can recognise this convention and tread this as a foreign key.
+        public byte MembershipTypeId { get; set; }
     }
 }
