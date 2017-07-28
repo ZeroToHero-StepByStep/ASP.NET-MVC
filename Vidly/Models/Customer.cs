f64e03dba10e3b0e699c1eb9bca19d4d7f1672ec
Pei <jzha471@aucklanduni.ac.nz>
2017-07-28 13:19:10 +1200

    52. Data Annotation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,7 +8,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         //this two approaches are overriding default conventions but with some disadvantages
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
@@ -17,10 +17,12 @@ namespace Vidly.Models
         public DateTime? BirthDate { get; set; }
         //MembershipType is called a naviation property becuase it allows us 
         //to navigate  from one type to another. In this case : Custoemr->Membershiptype
+      
         public MembershipType MembershipType { get; set; }
         //sometime for optimization , we don't want to load the entire Membershiptype of the object 
         //we may only need a foreign key so we can add another property here : MembershpTypeId 
         //EF can recognise this convention and tread this as a foreign key.
+        [Required(ErrorMessage = "Please choose a membership type")]
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
