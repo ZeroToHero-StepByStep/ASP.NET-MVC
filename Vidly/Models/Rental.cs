e2d07ca1cf25bd450c907313def8d54a685dc05f
Pei <jzha471@aucklanduni.ac.nz>
2017-08-01 16:43:23 +1200

    115. Domain modeling ( create Rental model )

---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        public int  MovieId { get; set; }
+
+        [ForeignKey("MovieId")]
+        public Movie Movie { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        public DateTime DateRented { get; set; }
+
+        public DateTime? DateReturned { get; set; }
+    }
+}
